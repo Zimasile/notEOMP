1ea21718381ae953a9e4ac887b7e293b5024f9f8
Amaru <114737080+Zimasile@users.noreply.github.com>
2023-02-23 09:12:58 +0200

    update

--- middleware/ErrorHandling.js
+++ middleware/ErrorHandling.js
@@ -1,45 +1,12 @@
-const express = require('express');
-// route
-const route = require('./controller');
-
-// cors
-const cors = require('cors');
-
-// port
-const port = parseInt(process.env.PORT) || 4000;
-
-// Express app
-const app = express();
-
-// Middleware
-const {errorHandling} = require('./middleware/ErrorHandling');
-
-//
-const cookieParser = require('cookie-parser');
-/*
-express.json: setting the content-type to application/json
-bodyParser.urlencoded( {extended: true} ): Object will contain
-values of any type instead of just a string
-*/
-app.use((req, res, next)=> {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-        res.header("Access-Control-Allow-Credentials", "true")
-        res.header("Access-Control-Allow-Methods", "*")
-        res.header("Access-Control-Allow-Headers", "*")
-        next();
-});
-app.use(route);
-app.use(
-    cors(),
-    cookieParser(),
-    express.json,
-    express.urlencoded({extended: false})
-)
-
-// Server is running
-app.listen(port, ()=> {
-    console.log(`Server is running`)
-});
-
-// Handling all errors
-app.use(errorHandling);
+function errorHandling(err, req, res, next) {
+    if(err) {
+        const status = err.status || 500;
+        res.status(status).json(
+            {
+                status: status,
+                err: "An error occurred. Please try again later."
+            });
+    }
+    next();
+}
+module.exports = {errorHandling};
